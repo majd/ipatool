067d53c64035082d2a278a766463b28761538e65
Majd Alfhaily <majd@alfhaily.me>
2021-05-22 11:02:42 +0200

    Add parameters to download command

--- Source/Commands/Download.swift
+++ Source/Commands/Download.swift
@@ -8,5 +8,25 @@
 import ArgumentParser
 
 struct Download: ParsableCommand {
+    static var configuration: CommandConfiguration {
+        return .init(abstract: "Download (encrypted) iOS app packages from the App Store.")
+    }
 
+    @Option(name: [.short, .long], help: "The bundle identifier of the target iOS app.")
+    private var bundleIdentifier: String
+
+    @Option(name: [.short, .long], help: "The email address for the Apple ID.")
+    private var email: String?
+
+    @Option(name: [.short, .long], help: "The password for the Apple ID.")
+    private var password: String?
+
+    @Option(name: [.short, .customLong("output")], help: "The path for saving the iOS app package.")
+    private var outputPath: String
+}
+
+extension Download {
+    func run() throws {
+        
+    }
 }
