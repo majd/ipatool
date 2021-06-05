b8e05c8e10b6118177121d44e31294773b1afa29
nthumann <me@n-thumann.de>
2021-06-05 23:26:14 +0200

    Add support for specifying country

--- Source/iTunes/iTunesRequest.swift
+++ Source/iTunes/iTunesRequest.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum iTunesRequest {
-    case search(term: String, limit: Int)
-    case lookup(bundleIdentifier: String)
+    case search(term: String, limit: Int, country: String)
+    case lookup(bundleIdentifier: String, country: String)
 }
 
 extension iTunesRequest: HTTPRequest {
@@ -28,10 +28,10 @@ extension iTunesRequest: HTTPRequest {
 
     var payload: HTTPPayload? {
         switch self {
-        case let .lookup(bundleIdentifier):
-            return .urlEncoding(["media": "software", "bundleId": bundleIdentifier, "limit": "1"])
-        case let .search(term, limit):
-            return .urlEncoding(["media": "software", "term": term, "limit": "\(limit)"])
+        case let .lookup(bundleIdentifier, country):
+            return .urlEncoding(["media": "software", "bundleId": bundleIdentifier, "limit": "1", "country": country])
+        case let .search(term, limit, country):
+            return .urlEncoding(["media": "software", "term": term, "limit": "\(limit)", "country": country])
         }
     }
 }
