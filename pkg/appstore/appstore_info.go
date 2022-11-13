a5a3f50f9840044312e49cc949f439e1121643cb
Majd Alfhaily <majd@alfhaily.me>
2022-11-13 12:35:52 +0100

    Implement appstore auth logic

--- pkg/appstore/appstore_info.go
+++ pkg/appstore/appstore_info.go
@@ -1,1 +1,28 @@
 package appstore
+
+import (
+	"encoding/json"
+	"github.com/majd/ipatool/pkg/log"
+	"github.com/pkg/errors"
+)
+
+func (a *appstore) Info() error {
+	data, err := a.keychain.Get("account")
+	if err != nil {
+		return errors.Wrap(err, "account was not found")
+	}
+
+	var account Account
+	err = json.Unmarshal(data, &account)
+	if err != nil {
+		return errors.Wrap(err, "failed to unmarshall account data")
+	}
+
+	log.Info().
+		Str("name", account.Name).
+		Str("email", account.Email).
+		Bool("succes", true).
+		Send()
+
+	return nil
+}
--- pkg/appstore/appstore_revoke_test.go
+++ pkg/appstore/appstore_revoke_test.go
@@ -1,1 +1,64 @@
 package appstore
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/majd/ipatool/pkg/keychain"
+	"github.com/majd/ipatool/pkg/log"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+)
+
+var _ = Describe("AppStore (Revoke)", Ordered, func() {
+	var (
+		ctrl         *gomock.Controller
+		appstore     AppStore
+		mockKeychain *keychain.MockKeychain
+	)
+
+	BeforeAll(func() {
+		log.Logger = log.Output(log.NewWriter()).Level(zerolog.Disabled)
+	})
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		mockKeychain = keychain.NewMockKeychain(ctrl)
+		appstore = NewAppStore(&Args{
+			Keychain: mockKeychain,
+		})
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	When("keychain returns error", func() {
+		var testErr = errors.New("test error")
+
+		BeforeEach(func() {
+			mockKeychain.EXPECT().
+				Remove("account").
+				Return(testErr)
+		})
+
+		It("returns wrapped error", func() {
+			err := appstore.Revoke()
+			Expect(err).To(MatchError(ContainSubstring(testErr.Error())))
+			Expect(err).To(MatchError(ContainSubstring("failed to revoke auth credentials")))
+		})
+	})
+
+	When("keychain removes item", func() {
+		BeforeEach(func() {
+			mockKeychain.EXPECT().
+				Remove("account").
+				Return(nil)
+		})
+
+		It("returns data", func() {
+			err := appstore.Revoke()
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
