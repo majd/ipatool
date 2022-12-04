4376fafbea4d9afe1374afc6b6d79a3018980c6e
Majd Alfhaily <majd@alfhaily.me>
2022-12-04 15:45:54 +0100

    Wire CLI commands with logic

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,1 +1,16 @@
 package cmd
+
+import "github.com/thediveo/enumflag/v2"
+
+type OutputFormat enumflag.Flag
+
+const (
+	OutputFormatText OutputFormat = iota
+	OutputFormatJSON
+)
+
+const (
+	ConfigDirectoryName = ".ipatool"
+	CookieJarFileName   = "cookies"
+	KeychainServiceName = "ipatool-auth.service"
+)
